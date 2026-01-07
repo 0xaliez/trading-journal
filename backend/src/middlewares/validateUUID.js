36832f5951190bb8344e34c8a77e3748ee65355f
Ali Asghar <k240780@nu.edu.pk>
2026-01-07 18:12:20 +0500

    Validate UUID Middleware

---
+++ backend/src/middlewares/validateUUID.js
@@ -0,0 +1,14 @@
+export function validateUUID(paramName) {
+  return (req, res, next) => {
+    const value = req.params[paramName] || req.body[paramName];
+
+    const uuidRegex =
+      /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+
+    if (!value || !uuidRegex.test(value)) {
+      return res.status(400).json({ error: `Invalid ${paramName}` });
+    }
+
+    next();
+  };
+}
